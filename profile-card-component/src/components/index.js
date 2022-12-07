7e7938aa5f2c20e24fabf0581caa1c562ec6343d
Axe10rellana <axelorellana550@gmail.com>
2022-12-07 08:00:45 -0300

    profile-card-component

--- profile-card-component/src/components/index.js
+++ profile-card-component/src/components/index.js
@@ -1,9 +1,7 @@
 //icons
-import PatternBottomIcon from "./Icons/PatternBottomIcon";
 import PatternCardIcon from "./Icons/PatternCardIcon";
-import PatternTopIcon from "./Icons/PatternTopIcon";
 
 //components
 import ProfileCardMain from "./ProfileCard/ProfileCardMain";
 
-export { PatternBottomIcon, PatternCardIcon, PatternTopIcon, ProfileCardMain };
+export { PatternCardIcon, ProfileCardMain };
